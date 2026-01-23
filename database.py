309d8563d6a6c8cf602595eb443286fd11226755
Zayaa7n <muhammedzayan204@gmail.com>
2026-01-23 16:15:51 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def init_db():
+    db = sqlite3.connect("airline.db")
+    with open("schema.sql") as f:
+        db.executescript(f.read())
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    init_db()
